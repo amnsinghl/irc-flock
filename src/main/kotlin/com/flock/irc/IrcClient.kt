ed5a633ab130fd87b560c66e7dcb54861aa41ac1
Aman Singhal <aman.si@riva.co>
2018-01-06 18:20:36 +0530

    sync

--- src/main/kotlin/com/flock/irc/IrcClient.kt
+++ src/main/kotlin/com/flock/irc/IrcClient.kt
@@ -2,23 +2,63 @@ package com.flock.irc
 
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.SettableFuture
+import org.json.JSONObject
 import org.kitteh.irc.client.library.Client
 import org.kitteh.irc.client.library.feature.auth.SaslEcdsaNist256PChallenge
 import org.kitteh.irc.client.library.feature.auth.SaslPlain
 import redis.clients.jedis.Jedis
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.ECPublicKey
 
-class IrcClient(private val userId: String, private val store: Jedis) {
-    val client = Client.builder().serverHost("irc.freenode.net").build()
+//@Throws(Exception::class)
+//fun getHTML(urlToRead: String): String {
+//    val result = StringBuilder()
+//    val url = URL(urlToRead)
+//    val conn = url.openConnection() as HttpURLConnection
+//    conn.setRequestMethod("GET")
+//    val rd = BufferedReader(InputStreamReader(conn.getInputStream()))
+//    var line: String?
+//    while (true) {
+//        line = rd.readLine()
+//        if (line == null) break
+//        result.append(line)
+//    }
+//    rd.close()
+//    return result.toString()
+//}
+//
+//fun generateName(): String {
+//    var s: String? = null
+//    try {
+//        s = getHTML("https://randomuser.me/api/")
+//    } catch (e: Exception) {
+//        e.printStackTrace()
+//    }
+//
+//    println("str  " + s!!)
+//    val obj = JSONObject(s)
+//    val name = obj.getJSONArray("results").getJSONObject(0).getJSONObject("name")
+//    val nam = name.getString("first")
+//    return nam.replace("[^a-zA-Z]".toRegex(), "")
+//}
 
-    fun connect(): ListenableFuture<Void> {
+class IrcClient(private val userId: String, listener: Main.IrcListener, private val store: Jedis) {
+    val client = Client.builder().serverHost("irc.freenode.net").build().apply {
+        eventManager.registerEventListener(listener)
+    }
+
+    fun connect(): ListenableFuture<IrcClient> {
+        client.nick = store[userId + "ircUserName"]
         client.connect()
-        val future = SettableFuture.create<Void>()
+        val future = SettableFuture.create<IrcClient>()
         client.setInputListener { it ->
             println(it)
             if (it.contains("MOTD") || it.contains("authentication successful")) {
-                future.set(null)
+                future.set(this)
             }
         }
         client.setOutputListener { it -> println(it) }
@@ -27,10 +67,12 @@ class IrcClient(private val userId: String, private val store: Jedis) {
 
     fun authenticateViaSasl() {
         val (ecPublicKey, ecPrivateKey) = getKey()
-        client.authManager.addProtocol(SaslEcdsaNist256PChallenge(client, "username", ecPrivateKey))
+        val ircUserName = store[userId + "ircUserName"]
+        client.authManager.addProtocol(SaslEcdsaNist256PChallenge(client, ircUserName, ecPrivateKey))
     }
 
     fun setUserNamePass(userName: String, password: String) {
+        store[userId + "ircUserName"] = userName
         client.authManager.addProtocol(SaslPlain(client, userName, password))
     }
 
@@ -40,7 +82,11 @@ class IrcClient(private val userId: String, private val store: Jedis) {
 
     fun setPublicKey() {
         val (ecPublicKey, ecPrivateKey) = getKey()
-        client.sendRawLine("/msg NickServ SET PUBKEY " + SaslEcdsaNist256PChallenge.getCompressedBase64PublicKey(ecPublicKey))
+        client.sendRawLine("asfdfwefasdfasd")
+        client.sendMessage("NickServ", "SET PUBKEY " + SaslEcdsaNist256PChallenge.getCompressedBase64PublicKey(ecPublicKey))
+    }
+
+    fun joinChannel() {
     }
 
     fun getKey(): Pair<ECPublicKey, ECPrivateKey> {
@@ -53,4 +99,5 @@ class IrcClient(private val userId: String, private val store: Jedis) {
             ecKeyPair.public to ecKeyPair.private
         }
     }
+
 }
--- src/main/kotlin/com/flock/irc/Main.kt
+++ src/main/kotlin/com/flock/irc/Main.kt
@@ -1,25 +1,25 @@
 package com.flock.irc
 
 import com.flock.EventHandlerClient
+import com.flock.api.Chat
+import com.flock.model.SendAs
+import com.flock.model.SendMessageOptionalParams
 import com.flock.model.ToastMessage
 import com.google.common.util.concurrent.FutureCallback
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
-import org.kitteh.irc.client.library.Client
-import org.kitteh.irc.client.library.feature.auth.SaslEcdsaNist256PChallenge
-import org.kitteh.irc.client.library.feature.auth.SaslPlain
+import net.engio.mbassy.listener.Handler
+import net.engio.mbassy.listener.Invoke
+import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent
+import org.kitteh.irc.client.library.event.client.ClientConnectionEstablishedEvent
 import redis.clients.jedis.Jedis
 import spark.Spark
 import java.lang.Exception
-import java.security.interfaces.ECPrivateKey
-import java.security.interfaces.ECPublicKey
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class Main {
     private val store: Jedis = Jedis("127.0.0.1")
     private val eventHandler: EventHandlerClient = EventHandlerClient("05bf2a80-19e8-46bc-84de-62c24e667836", "b9c5a695-0855-4c30-a4f4-c8da4c89cd53")
-    private val ircClientMap: Map<String, Client> = HashMap()
+    private val ircClientMap: MutableMap<String, IrcClient> = HashMap()
 
     init {
         println("ping " + store.ping())
@@ -27,35 +27,55 @@ class Main {
     }
 
     fun startListeningToEvents() {
-        eventHandler.setClientSlashCommandListener { it ->
-            println(it.command + " " + it.text)
-            val commandSplit = it.text.split(delimiters = " ", limit = 2)
-            val command = commandSplit[0]
-            val text = commandSplit[1]
+        eventHandler.setClientSlashCommandListener { slash ->
+            println(slash.command + " " + slash.text)
+            val commandSplit = slash.text.split(delimiters = " ", limit = 2)
+            val command = commandSplit[0].toLowerCase()
             when (command) {
                 "login" -> {
+                    val text = commandSplit[1]
                     val split = text.split(" ")
-                    register(it.userId, split[0], split[1])
-                    ToastMessage().text("Login successful")
+                    register(slash.userId, split[0], split[1]).onSuccess { client ->
+                        sendSelfVisiblefMessage(slash.userId, slash.chat, "Login successful")
+                        client!!.setPublicKey()
+                    }
+                }
+                "authenticate" -> {
+                    authenticate(slash.userId).onSuccess {
+                        sendSelfVisiblefMessage(slash.userId, slash.chat, "Authentication successful")
+                    }
                 }
                 "sendmessage" -> {
+                    val text = commandSplit[1]
+                    getIrcClient(slash.userId).onSuccess { ircClient ->
+                        val channelName = store[slash.chat + "channelName"]
+                        ircClient!!.client.addChannel(channelName)
+                        ircClient.client.sendMessage(channelName, text)
+                    }
 
-                    ToastMessage()
                 }
-//                "setChannel" -> {
-//
-//                }
-                else -> {
-                    ToastMessage().text("Unknown command")
+                "setchannel" -> {
+                    if (store.exists(slash.chat + "channelName")) {
+                        return@setClientSlashCommandListener ToastMessage().text("This channel is already linked with ${store[slash.chat + "channelName"]}")
+                    }
+                    val channelName = commandSplit[1]
+                    getIrcClient(slash.userId).onSuccess { ircClient ->
+                        ircClient!!.client.addChannel(channelName)
+                        store[slash.userId + channelName] = slash.chat
+                        store[slash.chat + "channelName"] = channelName
+                    }
+                }
+                "help" -> {
+                    sendSelfVisiblefMessage(slash.userId, slash.chat, helpText)
                 }
             }
+            ToastMessage()
         }
         eventHandler.setAppInstallListener { it ->
             store[it.userId + "token"] = it.token
         }
         Spark.port(4567)
         Spark.post("/event") { req, res ->
-            Thread.sleep(2000)
             println("event received")
             try {
                 eventHandler.handleRequest(req.raw(), res.raw())
@@ -65,31 +85,73 @@ class Main {
         }
     }
 
-    fun setChannel(ircChannelName: String, flockChannelId: String) {
-
-    }
-
-//    fun getClient() {
-//        if(ir)
-//    }
-
-    fun register(userId: String, userName: String, password: String) {
-        val ircClient = IrcClient(userId, store)
-//        ircClient.setUserNamePass(userName, password)
-        ircClient.connect().onSuccess {
-            ircClient.setPublicKey()
+    fun getIrcClient(userId: String): ListenableFuture<IrcClient> {
+        return if (ircClientMap.containsKey(userId)) {
+            Futures.immediateFuture(ircClientMap[userId])
+        } else {
+            authenticate(userId)
         }
     }
+
+    private val helpText = StringBuilder().apply {
+        appendln("/irc login <username> <password>")
+        appendln("/irc authenticate")
+        appendln("/irc sendmessage <message>")
+        appendln("/irc setchannel <irc channel name>")
+    }.toString()
+
+
+    fun sendSelfVisiblefMessage(userId: String, channelId: String, text: String) {
+        Chat.sendMessage(store[userId + "token"], channelId, text,
+                SendMessageOptionalParams().visibleTo(listOf(userId))
+                        .sendAs(SendAs().name("Irc Bot").profileImage("https://blog.openshift.com/wp-content/uploads/imported/alphie_hello_cloud.png")))
+    }
+
+
+    fun register(userId: String, userName: String, password: String): ListenableFuture<IrcClient> {
+        val ircClient = IrcClient(userId, IrcListener(userId, store), store)
+        ircClient.setUserNamePass(userName, password)
+        ircClientMap[userId] = ircClient
+        return ircClient.connect()
+    }
+
+    fun authenticate(userId: String): ListenableFuture<IrcClient> {
+        val ircClient = IrcClient(userId, IrcListener(userId, store), store)
+        ircClient.authenticateViaSasl()
+        ircClientMap[userId] = ircClient
+        return ircClient.connect()
+    }
+
+    class IrcListener(val userId: String, val store: Jedis) {
+
+
+        @Handler(delivery = Invoke.Asynchronously)
+        fun onConnectedEvent(v: ClientConnectionEstablishedEvent) {
+            val i = 1
+        }
+
+        @Handler(delivery = Invoke.Asynchronously)
+        fun onChannelMessageEvent(v: ChannelMessageEvent) {
+            if (store.exists(userId + v.channel.name)) {
+                Chat.sendMessage(store[userId + "token"], store[userId + v.channel.name], v.message, SendMessageOptionalParams().sendAs(getSendas(v.actor.nick)))
+            }
+        }
+
+        fun getSendas(name: String): SendAs {
+            return SendAs().name(name).profileImage("https://api.adorable.io/avatars/100/$name.png")
+        }
+    }
+
 }
 
 
-fun <U> ListenableFuture<U>.onSuccess(runnable: (U) -> Unit): ListenableFuture<U> =
+fun <U> ListenableFuture<U>.onSuccess(runnable: (U?) -> Unit): ListenableFuture<U> =
         this.apply { addCallback(success = runnable) }
 
 fun <U> ListenableFuture<U>.onFailure(runnable: (Throwable) -> Unit): ListenableFuture<U> =
         this.apply { addCallback(failure = runnable) }
 
-fun <U> ListenableFuture<U>.addCallback(success: (U) -> Unit = {},
+fun <U> ListenableFuture<U>.addCallback(success: (U?) -> Unit = {},
                                         failure: (Throwable) -> Unit = {}) {
     Futures.addCallback(this, object : FutureCallback<U> {
         override fun onFailure(p0: Throwable) {
@@ -97,11 +159,7 @@ fun <U> ListenableFuture<U>.addCallback(success: (U) -> Unit = {},
         }
 
         override fun onSuccess(p0: U?) {
-            if (p0 != null) {
-                success(p0)
-            } else {
-                failure(Throwable("null success"))
-            }
+            success(p0)
         }
     })
 }
